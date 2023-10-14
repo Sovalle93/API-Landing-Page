5b4133f1d9285ac1861bb1762eaacee4de175117
Sovalle93 <sovalleg93@gmail.com>
2023-10-14 16:33:06 -0300

    actualizacion de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Header from './components/Header'
 import Buscador from './components/Buscador'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+
 
 function App() {
 
